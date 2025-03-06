fce6a1cdb443cb023c2662b778fa06d3fa84c5b5
tibeesdev <tibeesdev@gmail.com>
2025-03-06 18:26:35 +0800

    menambahkan database model

--- lib/database_handler/database_instance.dart
+++ lib/database_handler/database_instance.dart
@@ -1,5 +1,55 @@
+import 'dart:io';
+
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class DatabaseInstance {
-  
+  // informasi database
+  final String nama_database = 'kasir.db';
+  final String versi_database = '1';
+
+  // informasi tabel
+  final String tabel_produk = 'Produk';
+  final String tabel_transaksi = 'Transaksi';
+  final String tabel_produk_transaksi = 'ProdukTransaksi';
+
+  // kolom produk
+  final String produk_id = 'id';
+  final String produk_nama = 'nama_barang';
+  final String produk_kode = 'kode_barang';
+  final String produk_harga = 'harga_barang';
+  final String produk_modal = 'modal_barang';
+  final String produk_stok = 'stok_barang';
+
+  // kolom transaksi
+  final String transaksi_id = 'id';
+  final String transaksi_kode = 'kode_transaksi';
+  final String transaksi_catatan = 'catatan_transaksi';
+  final String transaksi_total_modal = 'total_modal';
+  final String transaksi_total_harga = 'total_harga';
+  final String transaksi_total_keuntungan = 'total_keuntungan';
+
+  // kolom transaksi produk
+  final String transaksi_produk_id = 'id';
+  final String transaksi_produk_jumlah_item = 'jumlah_item';
+  final String transaksi_produk_kode_barang = 'kode_barang';
+  final String transaksi_produk_kode_transaksi = 'kode_transaksi';
+
+  // fungsi
+  // inisiasi database
+  Database? _database;
+  Future<Database> database() async {
+    if (_database != null) {
+      return _database!;
+    } else {
+      _database = await _initDatabase();
+      return _database!;
+    }
+  }
+
+  // inisiasi
+  Future _initDatabase() async {
+    Directory documentDirectory = await getApplicationDocumentsDirectory();
+  }
 }
--- lib/database_handler/database_model.dart
+++ lib/database_handler/database_model.dart
@@ -24,3 +24,53 @@ class ProductsModel {
     );
   }
 }
+
+// model untuk transaksi
+class TransactionsModel {
+  int? id, total_modal, total_harga, total_keuntungan;
+  String? catatan_transaksi, kode_transaksi;
+
+  TransactionsModel({
+    this.id,
+    this.total_modal,
+    this.total_harga,
+    this.total_keuntungan,
+    this.catatan_transaksi,
+    this.kode_transaksi,
+  });
+
+  // factory
+  factory TransactionsModel.fromJson(Map<String, dynamic> json) {
+    return TransactionsModel(
+      id: json['id'],
+      total_modal: json['total_modal'],
+      total_harga: json['total_harga'],
+      total_keuntungan: json['total_keuntungan'],
+      catatan_transaksi: json['catatan_transaksi'],
+      kode_transaksi: json['kode_transaksi'],
+    );
+  }
+}
+
+// model untuk produk
+class ProductsTransactionsModel {
+  int? id, jumlah_item;
+  String? kode_barang, kode_transaksi;
+
+  ProductsTransactionsModel({
+    this.id,
+    this.jumlah_item,
+    this.kode_barang,
+    this.kode_transaksi,
+  });
+
+  // factory
+  factory ProductsTransactionsModel.fromJson(Map<String, dynamic> json) {
+    return ProductsTransactionsModel(
+      id: json['id'],
+      jumlah_item: json['jumlah_item'],
+      kode_barang: json['kode_barang'],
+      kode_transaksi: json['kode_transaksi'],
+    );
+  }
+}
